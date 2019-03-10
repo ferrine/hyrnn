247722bbd2836256e7e4cec2617cddb632d42784
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-03-10 12:03:18 +0300

    use wget

--- download.sh
+++ download.sh
@@ -2,11 +2,11 @@
 
 function fetch() {
     mkdir -p data/prefix_$1_dataset
-    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/dev > data/prefix_$1_dataset/dev
-    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/id_to_word > data/prefix_$1_dataset/id_to_word
-    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/test > data/prefix_$1_dataset/test
-    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/train > data/prefix_$1_dataset/train
-    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/word_to_id > data/prefix_$1_dataset/word_to_id
+    wget https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/dev -O data/prefix_$1_dataset/dev
+    wget https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/id_to_word -O data/prefix_$1_dataset/id_to_word
+    wget https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/test -O data/prefix_$1_dataset/test
+    wget https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/train -O data/prefix_$1_dataset/train
+    wget https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/word_to_id -O data/prefix_$1_dataset/word_to_id
 }
 
 fetch 10
