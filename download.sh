2620a61078b235d61b95c70b3e882ba6a77a35ef
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-03-10 11:40:55 +0300

    add download script

--- download.sh
+++ download.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+function fetch() {
+    mkdir data/prefix_$1_dataset
+    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/dev > data/prefix_$1_dataset/dev
+    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/id_to_word > data/prefix_$1_dataset/id_to_word
+    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/test > data/prefix_$1_dataset/test
+    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/train > data/prefix_$1_dataset/train
+    curl https://github.com/dalab/hyperbolic_nn/raw/master/prefix_$1_dataset/word_to_id > data/prefix_$1_dataset/word_to_id
+}
+
+fetch 10
+fetch 30
+fetch 50
